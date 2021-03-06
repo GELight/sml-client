a1b82461e1dbceff142d8611c325483bf017f3ef
Mario Linz <gelight@gmx.de>
2021-03-07 00:54:41 +0100

    Fix class exports for sml-client package

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
-export { default as ReliableTxtDocument } from "@gelight/sml";
-export { default as ReliableTxtException } from "@gelight/sml";
-export { default as ReliableTxtEncoding } from "@gelight/sml";
-export { default as ReliableTxtFile } from "@gelight/sml";
+import { ReliableTxtDocument } from "@gelight/sml";
+import { ReliableTxtException } from "@gelight/sml";
+import { ReliableTxtEncoding } from "@gelight/sml";
+import { ReliableTxtFile } from "@gelight/sml";
 
-export { default as SmlAttribute } from "@gelight/sml";
-export { default as SmlDocument } from "@gelight/sml";
-export { default as SmlElement } from "@gelight/sml";
-export { default as SmlNamedNode } from "@gelight/sml";
-export { default as SmlNode } from "@gelight/sml";
-export { default as SmlEmptyNode } from "@gelight/sml";
-export { default as SmlParserException } from "@gelight/sml";
+import { SmlAttribute } from "@gelight/sml";
+import { SmlDocument } from "@gelight/sml";
+import { SmlElement } from "@gelight/sml";
+import { SmlNamedNode } from "@gelight/sml";
+import { SmlNode } from "@gelight/sml";
+import { SmlEmptyNode } from "@gelight/sml";
+import { SmlParserException } from "@gelight/sml";
 
-export { default as WsvChar } from "@gelight/sml";
-export { default as WsvLine } from "@gelight/sml";
-export { default as WsvDocument } from "@gelight/sml";
-export { default as WsvParserException } from "@gelight/sml";
+import { WsvChar } from "@gelight/sml";
+import { WsvLine } from "@gelight/sml";
+import { WsvDocument } from "@gelight/sml";
+import { WsvParserException } from "@gelight/sml";
 
-export { default as StringUtil } from "@gelight/sml";
+import { StringUtil } from "@gelight/sml";
+
+export {
+    ReliableTxtDocument,
+    ReliableTxtException,
+    ReliableTxtEncoding,
+    ReliableTxtFile,
+
+    SmlAttribute,
+    SmlDocument,
+    SmlElement,
+    SmlNamedNode,
+    SmlNode,
+    SmlEmptyNode,
+    SmlParserException,
+
+    WsvChar,
+    WsvLine,
+    WsvDocument,
+    WsvParserException,
+
+    StringUtil
+};

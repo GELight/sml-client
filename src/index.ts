bb7bf120c9a9a4c2b6131ff993ef3e61ccc07042
Mario Linz <gelight@gmx.de>
2021-04-27 22:06:55 +0200

    Improve release script. Restructure imports and use specific npm packages.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-import { ReliableTxtDocument } from "@gelight/sml";
-import { ReliableTxtException } from "@gelight/sml";
-import { ReliableTxtEncoding } from "@gelight/sml";
-import { ReliableTxtFile } from "@gelight/sml";
+import {
+    ReliableTxtDocument,
+    ReliableTxtEncoding,
+    ReliableTxtException,
+    ReliableTxtFile
+} from "@gelight/reliabletxt";
 
-import { SmlAttribute } from "@gelight/sml";
-import { SmlDocument } from "@gelight/sml";
-import { SmlElement } from "@gelight/sml";
-import { SmlNamedNode } from "@gelight/sml";
-import { SmlNode } from "@gelight/sml";
-import { SmlEmptyNode } from "@gelight/sml";
-import { SmlParserException } from "@gelight/sml";
+import {
+    StringUtil,
+    WsvChar,
+    WsvDocument,
+    WsvDocumentLineIterator,
+    WsvLine,
+    WsvParserException
+} from "@gelight/whitespacesv";
+
+import {
+    SmlAttribute,
+    SmlDocument,
+    SmlElement,
+    SmlEmptyNode,
+    SmlNamedNode,
+    SmlNode,
+    SmlParserException,
+} from "@gelight/sml";
+
 import SmlRequest from "./sml/SmlRequest";
-
-import { WsvChar } from "@gelight/sml";
-import { WsvLine } from "@gelight/sml";
-import { WsvDocument } from "@gelight/sml";
-import { WsvParserException } from "@gelight/sml";
-
-import { StringUtil } from "@gelight/sml";
-
 console.warn("Experimental > SmlRequest: This feature is a work in progress. Don't depend on it yet. It may go through significant changes.");
 
 export {
@@ -40,6 +46,7 @@ export {
     WsvLine,
     WsvDocument,
     WsvParserException,
+    WsvDocumentLineIterator,
 
     StringUtil
 };
